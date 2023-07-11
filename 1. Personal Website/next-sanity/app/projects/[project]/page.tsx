50e3011f344dae82c6e653abb52c0059f7936995
pisces330 <pisces2978@gmail.com>
2023-07-11 13:52:54 +0900

    프로젝트 상세 페이지

--- 1. Personal Website/next-sanity/app/projects/[project]/page.tsx
+++ 1. Personal Website/next-sanity/app/projects/[project]/page.tsx
@@ -1,4 +1,6 @@
 import { getProject } from "@/sanity/sanity-utils";
+import { PortableText } from "@portabletext/react";
+import Image from "next/image";
 
 type Props = {
   params: { project: string };
@@ -8,5 +10,37 @@ export default async function Project({ params }: Props) {
   const slug = params.project;
   const project = await getProject(slug);
 
-  return <div>{project.name}</div>;
+  return (
+    <div>
+      <header className="flex justify-between items-center">
+        <h1 className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-5xl font-extrabold drop-shadow">
+          {project.name}
+        </h1>
+
+        <a
+          href={project.url}
+          title="View Project"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-gray-100 rounded-lg px-4 py-3 text-gray-700 whitespace-nowrap hover:bg-pink-500 hover:text-pink-100 transition"
+        >
+          View Project
+        </a>
+      </header>
+
+      {/* 콘텐츠 */}
+      <div className="text-lg text-gray-700 mt-5">
+        <PortableText value={project.content} />
+      </div>
+
+      {/* 이미지 */}
+      <Image
+        src={project.image}
+        alt={project.name}
+        width={1920}
+        height={1080}
+        className="mt-10 birder-2 border-gray-799 object-cover rounded-x1"
+      ></Image>
+    </div>
+  );
 }
