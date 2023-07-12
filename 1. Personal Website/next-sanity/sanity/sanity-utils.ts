25c84d40e8b2d505ee2cc43bc38fb89ec4813ebe
pisces330 <pisces2978@gmail.com>
2023-07-12 15:14:25 +0900

    Navbar 관련 API 추가

--- 1. Personal Website/next-sanity/sanity/sanity-utils.ts
+++ 1. Personal Website/next-sanity/sanity/sanity-utils.ts
@@ -1,8 +1,10 @@
-//데이터를 가져오는 모든 함수
+// 데이터를 가져오는 모든 함수
 import { Project } from "@/types/Project";
 import { createClient, groq } from "next-sanity";
 import clientConfig from "./config/client-conig";
+import { Page } from "@/types/Page";
 
+//모든 프로젝트 조회
 export async function getProjects(): Promise<Project[]> {
   return createClient(clientConfig).fetch(groq`*[_type == "project"]{
     _id,
@@ -14,7 +16,7 @@ export async function getProjects(): Promise<Project[]> {
     content
   }`);
 }
-
+// 특정 프로젝트 조회
 export async function getProject(slug: string): Promise<Project> {
   return createClient(clientConfig).fetch(
     groq`*[_type == "project" && slug.current == $slug][0]{
@@ -26,6 +28,33 @@ export async function getProject(slug: string): Promise<Project> {
       url,
       content
     }`,
+    { slug }
+  );
+}
+
+// nav 내용 불러오기
+export async function getPages(): Promise<Page[]> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page"]{
+      _id,
+      _createdAt,
+      title,
+      "slug": slug.current,
+    
+    
+    }`
+  );
+}
+// 특정 nav 값 불러오기 ex) About, Contact ...
+export async function getPage(slug: string): Promise<Page> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page" && slug.current == $slug][0]{
+    _id,
+    _createdAt,
+    title,
+    "slug": slug.current,
+    content
+  }`,
     { slug }
   );
 }
